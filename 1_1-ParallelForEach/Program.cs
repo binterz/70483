cb26a45674680d4cfdd1e4b01fc32298ab93b357
binterz <sean@ghostdesigns.co.uk>
2019-07-29 19:48:45 +0100

    skill 1_1 parallel loops

---
+++ 1_1-ParallelForEach/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _1_1_ParallelForEach
+{
+    class Program
+    {
+        static void WorkOnItem(object item)
+        {
+            Console.WriteLine("Started working on: " + item);
+            Thread.Sleep(100);
+            Console.WriteLine("Finished working on: " + item);
+        }
+
+        static void Main(string[] args)
+        {
+            var items = Enumerable.Range(0, 500);
+            Parallel.ForEach(items, item =>
+            {
+                WorkOnItem(item);
+            });
+            Console.WriteLine("Finished working on all items.");
+            Console.ReadKey();
+        }
+    }
+}
